d48a16f006607107cfb3a1e9f5186b5a9086f6c7
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-30 16:33:14 +0300

    Update play.py

--- AlexaMusic/plugins/play/play.py
+++ AlexaMusic/plugins/play/play.py
@@ -42,7 +42,11 @@ from AlexaMusic.utils.database import is_served_user
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 
-@app.on_message(filters.command([PLAY_COMMAND], prefixes=["/", "!", "%", ",", "", ".", "@", "#"])  & ~BANNED_USERS)
+@app.on_message(
+ filters.command(PLAY_COMMAND,"")
+    & filters.private
+    & ~filters.edited
+    & ~BANNED_USERS)
 @PlayWrapper
 async def play_commnd(
     client,
