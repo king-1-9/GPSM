f5ee04e10b31919b6aab9b1e5625bcd25c4aa1b8
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-04-04 20:14:12 +0300

    Update language.py

--- AlexaMusic/utils/decorators/language.py
+++ AlexaMusic/utils/decorators/language.py
@@ -31,7 +31,7 @@ def language(mystic):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("ar")
         return await mystic(_, message, language)
 
     return wrapper
@@ -49,7 +49,7 @@ def languageCB(mystic):
             language = await get_lang(CallbackQuery.message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("ar")
         return await mystic(_, CallbackQuery, language)
 
     return wrapper
@@ -61,7 +61,7 @@ def LanguageStart(mystic):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("ar")
         return await mystic(_, message, language)
 
     return wrapper
