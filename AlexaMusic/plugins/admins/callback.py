250892d704fba947baa70e726c4835fa6413c06c
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-04-04 13:28:55 +0300

    Update callback.py

--- AlexaMusic/plugins/admins/callback.py
+++ AlexaMusic/plugins/admins/callback.py
@@ -32,7 +32,11 @@ async def markup_panel(client, CallbackQuery: CallbackQuery, _):
     callback_request = callback_data.split(None, 1)[1]
     videoid, chat_id = callback_request.split("|")
     chat_id = CallbackQuery.message.chat.id
-    buttons = panel_markup_1(_, videoid, chat_id)
+    buttons = buttons = panel_markup_1(_,
+                            playing[0]["vidid"],
+                            chat_id,
+                            seconds_to_min(playing[0]["played"]),
+                            playing[0]["dur"],)
     try:
         await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
