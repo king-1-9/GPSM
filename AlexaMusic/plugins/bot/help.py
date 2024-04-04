973d2cb225b5541cda275d965f2527b8a083cd20
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-04-04 20:10:10 +0300

    Update help.py

--- AlexaMusic/plugins/bot/help.py
+++ AlexaMusic/plugins/bot/help.py
@@ -44,9 +44,11 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_, True)
         if update.message.photo:
-            await update.message.reply_text(_["help_1"], reply_markup=keyboard)
+            await update.edit_message_text(
+                _["help_1"], reply_markup=keyboard)
         else:
-            await update.edit_message_text(_["help_1"], reply_markup=keyboard)
+            await update.edit_message_text(
+                _["help_1"], reply_markup=keyboard)
     else:
         chat_id = update.chat.id
         if await is_commanddelete_on(update.chat.id):
@@ -60,7 +62,7 @@ async def helper_private(
         await update.reply_sticker("CAACAgUAAxkBAAIjVmKPYTFByKZlCo9d8mUv8QVAJEw7AAL9BQACiy14VGoQxOCDfE1KJAQ")
         await update.reply_photo(
             photo=config.START_IMG_URL,
-            caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
+            caption=_["help_1"], reply_markup=keyboard)
 
 
 @app.on_message(filters.command(HELP_COMMAND) & filters.group & ~BANNED_USERS)
