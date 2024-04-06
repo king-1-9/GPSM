cd72c34437e20824edc73737bdec99385646ca1e
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-04-06 20:46:03 +0300

    Update seeker.py

--- AlexaMusic/plugins/misc/seeker.py
+++ AlexaMusic/plugins/misc/seeker.py
@@ -82,6 +82,17 @@ async def markup_timer():
                             seconds_to_min(playing[0]["played"]),
                             playing[0]["dur"],
                         )
+                        if markup == "stream"
+                        else telegram_markup_timer(
+                            _,
+                            chat_id,
+                            seconds_to_min(playing[0]["played"]),
+                            playing[0]["dur"],
+                        )
+                    )
+                    await mystic.edit_reply_markup(
+                        reply_markup=InlineKeyboardMarkup(buttons)
+                    )
                 except:
                     continue
             except:
@@ -89,4 +100,3 @@ async def markup_timer():
 
 
 asyncio.create_task(markup_timer())
-
