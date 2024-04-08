813c93edb04d295e48a245b02fc2d16fc81819d5
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-08 15:44:47 +0300

    Update auth.py

--- AlexaMusic/plugins/admins/auth.py
+++ AlexaMusic/plugins/admins/auth.py
@@ -31,7 +31,10 @@ UNAUTH_COMMAND = get_command("UNAUTH_COMMAND")
 AUTHUSERS_COMMAND = get_command("AUTHUSERS_COMMAND")
 
 
-@app.on_message(filters.command(AUTH_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+ filters.command(AUTH_COMMAND,"")
+    & filters.group
+    & ~BANNED_USERS)
 @AdminActual
 async def auth(client, message: Message, _):
     if not message.reply_to_message:
@@ -93,7 +96,10 @@ async def auth(client, message: Message, _):
         await message.reply_text(_["auth_3"])
 
 
-@app.on_message(filters.command(UNAUTH_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+ filters.command(UNAUTH_COMMAND,"")
+    & filters.group
+    & ~BANNED_USERS)
 @AdminActual
 async def unauthusers(client, message: Message, _):
     if not message.reply_to_message:
@@ -126,7 +132,10 @@ async def unauthusers(client, message: Message, _):
         return await message.reply_text(_["auth_5"])
 
 
-@app.on_message(filters.command(AUTHUSERS_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+ filters.command(AUTHUSERS_COMMAND,"")
+    & filters.group
+    & ~BANNED_USERS)
 @language
 async def authusers(client, message: Message, _):
     _playlist = await get_authuser_names(message.chat.id)
