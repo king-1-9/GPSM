3f3b369d83ca394f1303b7391b766ab38f2e6815
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-08 15:54:47 +0300

    Update resume.py

--- AlexaMusic/plugins/admins/resume.py
+++ AlexaMusic/plugins/admins/resume.py
@@ -24,7 +24,10 @@ from AlexaMusic.utils.decorators import AdminRightsCheck
 RESUME_COMMAND = get_command("RESUME_COMMAND")
 
 
-@app.on_message(filters.command(RESUME_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+ filters.command(RESUME_COMMAND,"")
+    & filters.group
+    & ~BANNED_USERS)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
@@ -32,6 +35,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"], disable_web_page_preview=True)
     await music_on(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "المشـرف"
     await Alexa.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), disable_web_page_preview=True
