bcf008778d3c177b12b0543673c0c7e7ebb188ed
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-08 15:52:44 +0300

    Update mute.py

--- AlexaMusic/plugins/admins/mute.py
+++ AlexaMusic/plugins/admins/mute.py
@@ -35,8 +35,8 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
     await mute_on(chat_id)
-    await Alexa.mute_stream(chat_id)
     user_mention = message.from_user.mention if message.from_user else "المشـرف"
+    await Alexa.mute_stream(chat_id)
     await message.reply_text(
         _["admin_6"].format(message.from_user.mention), disable_web_page_preview=True
     )
